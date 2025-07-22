9dcbaee16103855b5b6000b8969b158f42633acc
Guilherme Correa <guilhermecorrea@ucl.br>
2025-07-21 23:13:12 -0300

    21.07 feat: criando logs e Basket API

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Basket.API.Basket.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName) : ICommand<DeletBasketResult>;
+
+    public record DeletBasketResult(bool IsSuccess);
+
+    public class DeleteBasketCommandValidator : AbstractValidator<DeleteBasketCommand>
+    {
+        public DeleteBasketCommandValidator()
+        {
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        }
+    }
+
+    public class DeleteBasketHandler
+        : ICommandHandler<DeleteBasketCommand, DeletBasketResult>
+    {
+        public async Task<DeletBasketResult> Handle(DeleteBasketCommand request, CancellationToken cancellationToken)
+        {
+            //TODO: delete basket from database and cache
+
+            return new DeletBasketResult(true);
+        }
+    }
+}
